55cc1e4d8a7478178aa22ffb9ed19a8db976b5fa
Dushukhin Igor <dushuhinio@gmail.com>
2020-09-23 15:44:57 +0700

    create feed component: header

---
+++ src/Feed.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Feed.css';
+
+function Feed() {
+  return (
+    <div>
+      {/* Header */}
+      <div className="feed">
+        <h2>Home</h2>
+      </div>
+      {/* Tweetbox */}
+      {/* POst */}
+    </div>
+  );
+}
+
+export default Feed;
