5a21a4973df5ba749f996621aceb0a1ecb6672de
Dushukhin Igor <dushuhinio@gmail.com>
2020-09-23 15:45:17 +0700

    add feed component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import './App.css';
 import Sidebar from './Sidebar';
+import Feed from './Feed';
 
 function App() {
   return (
     <div className="App">
-      <h1>This Twitter clone</h1>
       {/* Sidebar */}
       <Sidebar />
       {/* Feed */}
+      <Feed />
       {/* Widgets */}
     </div>
   );
