4a04be26fa098c3bcbe92098cee39f4c29d600a9
Dushukhin Igor <dushuhinio@gmail.com>
2020-09-23 14:10:47 +0700

    create sidebar component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import './App.css';
+import Sidebar from './Sidebar';
 
 function App() {
   return (
     <div className="App">
       <h1>This Twitter clone</h1>
+      {/* Sidebar */}
+      <Sidebar />
+      {/* Feed */}
+      {/* Widgets */}
     </div>
   );
 }
---
+++ src/Sidebar.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import './Sidebar.css';
+import SidebarOption from './SidebarOption';
+
+import {
+  Twitter,
+  Home,
+  Search,
+  NotificationsNone,
+  BookmarkBorder,
+  ListAlt,
+  MailOutline,
+  MoreHoriz,
+  PermIdentity
+} from '@material-ui/icons';
+import { Button } from '@material-ui/core';
+
+function Sidebar() {
+  return (
+    <div className="sidebar">
+      <Twitter />
+
+      <SidebarOption active Icon={ Home } text="Home"/>
+      <SidebarOption Icon={ Search } text="Explore"/>
+      <SidebarOption Icon={ NotificationsNone } text="Notifications"/>
+      <SidebarOption Icon={ MailOutline } text="Messages"/>
+      <SidebarOption Icon={ BookmarkBorder } text="Bookmarks"/>
+      <SidebarOption Icon={ ListAlt } text="Lists"/>
+      <SidebarOption Icon={ PermIdentity } text="Profile"/>
+      <SidebarOption Icon={ MoreHoriz } text="More"/>
+
+      <Button variant="outlined" className="sidebar__tweet" fullWidth>
+        Tweet
+      </Button>
+    </div>
+  );
+}
+
+export default Sidebar;
